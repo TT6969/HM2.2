48c82024148710120458fd1b7d9bb4442c7bcae3
Anton Dorokhov <anton.dorohof@gmail.com>
2021-07-12 07:10:08 +0200

    sliders + label

--- HM2.2/ViewController.swift
+++ HM2.2/ViewController.swift
@@ -9,6 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var rgbLabel: UILabel!
+    
+    
+    
+    
+    
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
