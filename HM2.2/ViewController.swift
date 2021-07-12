ec63b55d474c2ee40e904bcc79dfd40888a0dad5
Anton Dorokhov <anton.dorohof@gmail.com>
2021-07-12 08:22:17 +0200

    added actions to sliders

--- HM2.2/ViewController.swift
+++ HM2.2/ViewController.swift
@@ -8,26 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-   
+    
+    //rgbLabel
     @IBOutlet weak var rgbLabel: UILabel!
     
-    
-    
-    
-    
-    
+    //rgbSliders
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     
     
     
+    
+    @IBAction func slidersActions(_ sender: Any) {
+        
+        rgbLabel.text = String(redSlider.value)
+        rgbLabel.text = String(greenSlider.value)
+        rgbLabel.text = String(blueSlider.value)
+        
+        self.rgbLabel.backgroundColor = UIColor(red:<#T##CGFloat#> (redSlider.value)/255,green:CGFloat(greenSlider.value)/255, blue: <#T##CGFloat#>(blueSlider.value)/255,alpha: 1)
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
