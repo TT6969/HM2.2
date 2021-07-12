bebd283b708feb7e8c8c6993d32ce20fb99aae93
Anton Dorokhov <anton.dorohof@gmail.com>
2021-07-12 08:45:21 +0200

    changed actions

--- HM2.2/ViewController.swift
+++ HM2.2/ViewController.swift
@@ -17,9 +17,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     
-    
-    
-    
     @IBAction func slidersActions(_ sender: Any) {
         
         rgbLabel.text = String(redSlider.value)
@@ -29,11 +26,10 @@ class ViewController: UIViewController {
         self.rgbLabel.backgroundColor = UIColor(red:CGFloat (redSlider.value)/255,green:CGFloat(greenSlider.value)/255, blue: CGFloat(blueSlider.value)/255,alpha: 1)
         
     }
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.rgbLabel.backgroundColor = .black
     }
     
     
